520908f445cf8e3de1d2c3e1d3dc58f825e3cab7
LevisNgigi <ngigilevis@gmail.com>
2022-09-08 02:45:49 -0700

    Tak 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,8 @@
  * main - prints the phrase "Programming is like building a multilingual puzzle
  * Return: 0 if executed properly, non-zero otherwise
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
